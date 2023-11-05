d33222cf2588ec32df9b1dfeb9cd6682cc90d672
laurecepatarata <149944649+laurecepatarata@users.noreply.github.com>
2023-11-05 21:07:09 +0800

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+       private static int instanceCount = 0;
+
+   
+    public MyClass() {
+        instanceCount++;
+    }
+
+    public static int getInstanceCount() {
+        return instanceCount;
+    }
+}
